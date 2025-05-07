cf1117bd8544a98f502632edc641dd1be36412df
red <pfcgm24@naver.com>
2025-05-07 22:12:32 +0900

    chore: Configure EFBoardDbContext with Dbset mappings

---
+++ Data/EFBoardDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EF_Board.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Board.Data
+{
+    public class EFBoardDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = default!;
+        public DbSet<Post> Posts { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFBoardDb;Trusted_Connection=True;");
+            }
+        }
+    }
+}
